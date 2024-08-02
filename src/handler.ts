81007d11176384172e5fb508385534ca3b2824a0
Juan Carlos Valdés <idevkingos@gmail.com>
2024-08-02 16:28:54 -0300

    initial commit

--- src/handler.ts
+++ src/handler.ts
@@ -2,7 +2,7 @@ export async function run(event, context, cb) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      region: "brasil",
+      region: "chile",
       utc: -3,
     }),
   };
