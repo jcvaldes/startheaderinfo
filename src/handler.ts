19fc09de8e32c3fd6da55f6b9c8d36c42e06f292
Juan Carlos Valdés <idevkingos@gmail.com>
2024-08-07 16:00:02 -0300

    initial commit

--- src/handler.ts
+++ src/handler.ts
@@ -1,15 +1,39 @@
 import { APIGatewayEvent, Context, Callback, APIGatewayProxyResult } from 'aws-lambda'
+// Lista de países válidos
+const validateCountries = [
+  'argentina',
+  'brasil',
+  'chile',
+  'colombia',
+  'costarica',
+  'dominicana',
+  'ecuador',
+  'guatemala',
+  'honduras',
+  'mexico',
+  'nicaragua',
+  'panama',
+]
+
+// Función para validar el nombre del país
+const countryValidate = (country) => {
+  return validateCountries.includes(country.toLowerCase())
+}
 
 export async function run(event: APIGatewayEvent, context: Context, cb: Callback): APIGatewayProxyResult {
   const { 'x-end-user-location': xEndUserLocation, 'x-end-user-time-zone': xEndUserTimeZone } = event.headers
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: {
-        region: xEndUserLocation,
-        utc: xEndUserTimeZone,
-      },
-    }),
+  // validaciones de region validas
+  if (countryValidate(xEndUserLocation)) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: {
+          region: xEndUserLocation,
+          utc: xEndUserTimeZone,
+        },
+      }),
+    }
+  } else {
+    return cb({ statusCode: 404, body: `El país ${xEndUserLocation} no está soportado` })
   }
 }
