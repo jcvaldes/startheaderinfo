1cf8ac5c18d4d263e47dbf00a4d6a580138ef501
Juan Carlos Valdés <idevkingos@gmail.com>
2024-08-07 12:39:07 -0300

    initial commit

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,15 @@
-import { APIGatewayEvent, Context, Callback, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayEvent, Context, Callback, APIGatewayProxyResult } from 'aws-lambda'
 
-export async function run(event: APIGatewayEvent, context: Context, cb: Callback):  APIGatewayProxyResult {
-  const { "x-end-user-location": xEndUserLocation, "x-end-user-time-zone": xEndUserTimeZone } = event.headers;
+export async function run(event: APIGatewayEvent, context: Context, cb: Callback): APIGatewayProxyResult {
+  const { 'x-end-user-location': xEndUserLocation, 'x-end-user-time-zone': xEndUserTimeZone } = event.headers
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      region: xEndUserLocation,
-      utc: xEndUserTimeZone,
+      data: {
+        region: xEndUserLocation,
+        utc: xEndUserTimeZone,
+      },
     }),
-  };
+  }
 }
